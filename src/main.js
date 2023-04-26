ad128681c77035bce4c1cc9eecae961710310901
luca-macedone <luca.macedone@outlook.it>
2023-04-26 13:31:31 +0200

    first commit

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import '/node_modules/bootstrap/dist/js/bootstrap.js'
+// import { library } from '@fortawesome/fontawesome-svg-core'
+// import {  } from '@fortawesome/free-solid-svg-icons'
+// import {  } from '@fortawesome/free-regular-svg-icons'
+// import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import App from './App.vue'
+
+createApp(App).mount('#app')
