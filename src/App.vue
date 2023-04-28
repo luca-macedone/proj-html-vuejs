07d9b10284eef7081a8e0c35669280d1e37640f0
luca-macedone <luca.macedone@outlook.it>
2023-04-28 15:35:53 +0200

    reemove issue propsdrilling

--- src/App.vue
+++ src/App.vue
@@ -43,62 +43,6 @@ export default {
 					badge: 'new',
 				},
 			],
-			footerLinks: [
-				[	// recent_posts
-					{
-						link_text: 'The best protein shake',
-						link_href: '#',
-					},
-					{
-						link_text: 'Ultimate cardio workout',
-						link_href: '#',
-					},
-					{
-						link_text: 'New juices available now',
-						link_href: '#',
-					},
-					{
-						link_text: 'Tips to find training partners',
-						link_href: '#',
-					},
-					{
-						link_text: '20 best healthy recipes',
-						link_href: '#',
-					},
-				],
-				[	// socials
-					{
-						social_name: 'Facebook',
-						icon_name: 'fa-facebook-f',
-						href: '#',
-					},
-					{
-						social_name: 'Twitter',
-						icon_name: 'fa-twitter',
-						href: '#',
-					},
-					{
-						social_name: 'YouTube',
-						icon_name: 'fa-youtube',
-						href: '#',
-					},
-					{
-						social_name: 'Instagram',
-						icon_name: 'fa-instagram',
-						href: '#',
-					},
-					{
-						social_name: 'LinkedIn',
-						icon_name: 'fa-linkedin-in',
-						href: '#',
-					},
-				],
-				{	// contacts
-					location: '4746 Tipple Road Michigan 48449',
-					mobile: '1.800.000.0000',
-					mail: 'info@your-company.com',
-				},
-			]
 		}
 	}
 }
@@ -108,7 +52,7 @@ export default {
 	<div>
 		<AppHeader :list="headerLinks"/>
 		<AppMain />
-		<AppFooter :list="footerLinks"/>
+		<AppFooter/>
 	</div>
 </template>
 
--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <AppFooterTop class="border-bottom" :list="list" />
+        <AppFooterTop class="border-bottom" :list="footerLinks" />
         <AppFooterBottom />
     </div>
 </template>
@@ -14,7 +14,66 @@ export default {
         AppFooterTop,
         AppFooterBottom,
     },
-    props: ['list'],
+    data(){
+        return {
+            footerLinks: [
+				[	// recent_posts
+					{
+						link_text: 'The best protein shake',
+						link_href: '#',
+					},
+					{
+						link_text: 'Ultimate cardio workout',
+						link_href: '#',
+					},
+					{
+						link_text: 'New juices available now',
+						link_href: '#',
+					},
+					{
+						link_text: 'Tips to find training partners',
+						link_href: '#',
+					},
+					{
+						link_text: '20 best healthy recipes',
+						link_href: '#',
+					},
+				],
+				[	// socials
+					{
+						social_name: 'Facebook',
+						icon_name: 'fa-facebook-f',
+						href: '#',
+					},
+					{
+						social_name: 'Twitter',
+						icon_name: 'fa-twitter',
+						href: '#',
+					},
+					{
+						social_name: 'YouTube',
+						icon_name: 'fa-youtube',
+						href: '#',
+					},
+					{
+						social_name: 'Instagram',
+						icon_name: 'fa-instagram',
+						href: '#',
+					},
+					{
+						social_name: 'LinkedIn',
+						icon_name: 'fa-linkedin-in',
+						href: '#',
+					},
+				],
+				{	// contacts
+					location: '4746 Tipple Road Michigan 48449',
+					mobile: '1.800.000.0000',
+					mail: 'info@your-company.com',
+				},
+			]
+        }
+    }
 }
 </script>
 
